dee5308731eb4e58ae5bfe7cd8514a2b2aae9211
Shuu <csaba19999@gmail.com>
2020-11-01 20:42:03 +0100

    Controller v3

--- backend/src/main/java/hu/uni/eku/tzs/controller/dto/GuestDTO.java
+++ backend/src/main/java/hu/uni/eku/tzs/controller/dto/GuestDTO.java
@@ -8,7 +8,7 @@ import java.time.LocalDateTime;
 @Data
 @Builder
 public class GuestDTO {
-    private int ID;
+    private int user_id;
     private WatchDTO watch;
     private TransactionDTO transaction;
     private LocalDateTime arrivalDateTime;
