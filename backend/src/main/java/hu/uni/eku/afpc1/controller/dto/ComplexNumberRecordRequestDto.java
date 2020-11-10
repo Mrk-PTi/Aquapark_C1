17607cf740daa71ce2de7e98cf2d3ff10df7a735
David <kissimond423@gmail.com>
2020-11-10 19:23:36 +0100

    ComplexNumber dto-k átnevezve

--- backend/src/main/java/hu/uni/eku/afpc1/controller/dto/ComplexNumberRecordRequestDto.java
+++ backend/src/main/java/hu/uni/eku/afpc1/controller/dto/ComplexNumberRecordRequestDto.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class ComplexNumberRecordRequestDTO {
+public class ComplexNumberRecordRequestDto {
 
     private double real;
     private double imag;
