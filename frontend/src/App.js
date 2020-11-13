1356bebbc87ebf576ba8f78dd9567426599c0cd3
Shuu <csaba19999@gmail.com>
2020-11-13 16:17:35 +0100

    App js módosítva, watch beleirva

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.scss';
-import ComplexNumberRecordingForm from "./component/ComplexNumberRecordingForm";
-import ComplexNumberList from "./component/ComplexNumberList";
+import WatchRecordingForm from "./component/WatchRecordingForm";
+import WatchList from "./component/WatchList";
 
 function App() {
   return (
@@ -9,8 +9,8 @@ function App() {
         <div className={"row"}>
             <div className={"col-md-3"}></div>
             <div className={"col-md-6"}>
-                <ComplexNumberRecordingForm/>
-                <ComplexNumberList/>
+                <WatchRecordingForm/>
+                <WatchList/>
             </div>
             <div className={"col-md-3"}></div>
         </div>
