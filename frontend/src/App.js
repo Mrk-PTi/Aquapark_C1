89431efddaee8d06fe91067fa9c230a9a54f5564
cipoleon <cipoka1@gmail.com>
2020-11-15 16:18:05 +0100

    Layout kész
    App.css létrehozva és App.js kiegészitve.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import './App.scss';
+import './App.css';
 import WatchRecordingForm from "./component/WatchRecordingForm";
 import WatchList from "./component/WatchList";
+import bg from "./video/bg.mp4";
 
 function App() {
   return (
     <div className={["App","container"]}>
-        <div className={"row"}>
-            <div className={"col-md-3"}></div>
-            <div className={"col-md-6"}>
-                <WatchRecordingForm/>
-                <WatchList/>
+
+        <video src={bg} autoPlay="true" loop="true" muted="true"/>
+        <div className="header">
+            <a href="#default" className="logo">Aquapark</a>
+            <div className="header-right">
+                <a className="active" href="#home">Home</a>
+                <a href="#contact">Contact</a>
+                <a href="#about">About</a>
             </div>
-            <div className={"col-md-3"}></div>
         </div>
+        <div id="content" className="round">
+            <WatchRecordingForm/>
+            <WatchList/>
+        </div>
+        <footer className="round">
+            <p>Made by group1</p>
+        </footer>
     </div>
   );
 }
