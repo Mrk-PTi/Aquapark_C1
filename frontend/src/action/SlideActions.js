f9ac71a5ab72cbc005fb9236d38a7b3526560a54
agnespapp0729 <agnespapp99@gmail.com>
2020-11-19 17:49:02 +0100

    SlideActions megírva

---
+++ frontend/src/action/SlideActions.js
@@ -0,0 +1,47 @@
+import axios from 'axios';
+import dispatcher from '../dispatcher/Dispatcher';
+import * as actionConstants from '../dispatcher/SlideActionConstants'
+
+export const recordSlide = ({slideId, slideExpense}) =>{
+    axios.post('/Slide/record',
+        {
+            slideId : slideId,
+            slideExpense : slideExpense
+        })
+        .then(() => {
+            fetchSlides();
+            dispatcher.dispatch({action : actionConstants.clearError});
+        })
+        .catch((err) => {
+            dispatcher.dispatch({
+                action : actionConstants.showError,
+                payload: `${err.response.status}-${err.response.statusText}: ${err.response.data.message}`
+            });
+            fetchSlides();
+        });
+}
+
+export const fetchSlides = () =>{
+
+    axios.get('/slide/').then((resp)=>{
+        dispatcher.dispatch({
+            action : actionConstants.refresh,
+            payload: resp.data
+        });
+    })
+}
+
+export const deleteSlide = (slideId) =>{
+    axios.delete(`/slide/${slideId}`)
+        .then(() => {
+            fetchSlides();
+            dispatcher.dispatch({action : actionConstants.clearError});
+        })
+        .catch((err) => {
+            dispatcher.dispatch({
+                action : actionConstants.showError,
+                payload: `${err.response.status}-${err.response.statusText}: ${err.response.data.message}`
+            });
+            fetchSlides();
+        });
+}
