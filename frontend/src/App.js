0e9b86fece3fd00a3b364acbe57a07aa999e5310
Aux Ciprián <60713159+cipoleon@users.noreply.github.com>
2021-03-24 00:41:14 +0100

    Lista kinézet elkészítve
    Lista kinézet elkészítve

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import './App.scss';
-import './App.css';
+import React from "react";
+import "./App.scss";
+import "./App.css";
 import WatchRecordingForm from "./component/WatchRecordingForm";
 import WatchList from "./component/WatchList";
 import SlideRecordingForm from "./component/SlideRecordingForm";
@@ -12,48 +12,53 @@ import PayRecordingForm from "./component/PayRecordingForm";
 import PayList from "./component/PayList";
 
 function App() {
-    function refreshPage() {
-        window.location.reload(false);
-    }
-    return (
-        <div className={["App","container"]}>
-            <video src={bg} autoPlay="true" loop="true" muted="true"/>
-            <div className="header">
-                <a href="#default" className="logo">Aquapark</a>
-                <div className="header-right">
-                    <a className="active" href="#home">Home</a>
-                    <a href="#contact">Contact</a>
-                    <a href="#about">About</a>
-                </div>
-            </div>
-            <div id="content" className="round">
-                <button id="refresh" onClick={()=> refreshPage()}>↺</button>
-                <div id="firstdiv">
-                    <GuestRecordingForm/>
-                </div>
-                <div id="firstdiv">
-                    <WatchRecordingForm/>
-                </div>
-                <div id="firstdiv">
-                    <SlideRecordingForm/>
-                </div>
-                <div id="firstdiv">
-                    <PayRecordingForm/>
-                </div>
-                <div id="messagediv">
-                    <GuestList/>
-                    <WatchList/>
-                    <SlideList/>
-                    <PayList/>
-                </div>
-
-            </div>
-            <footer className="round">
-                <p>Made by Group 1</p>
-                <p>All rights reserved.</p>
-            </footer>
+  function refreshPage() {
+    window.location.reload(false);
+  }
+  return (
+    <div className={["App", "container"]}>
+      <video src={bg} autoPlay="true" loop="true" muted="true" />
+      <div className="header">
+        <a href="#default" className="logo">
+          Aquapark
+        </a>
+        <div className="header-right">
+          <a className="active" href="#home">
+            Home
+          </a>
+          <a href="#contact">Contact</a>
+          <a href="#about">About</a>
         </div>
-    );
+      </div>
+      <div id="content" className="round">
+        <button id="refresh" onClick={() => refreshPage()}>
+          ↺
+        </button>
+        <div id="firstdiv">
+          <GuestRecordingForm />
+        </div>
+        <div id="firstdiv">
+          <WatchRecordingForm />
+        </div>
+        <div id="firstdiv">
+          <SlideRecordingForm />
+        </div>
+        <div id="firstdiv">
+          <PayRecordingForm />
+        </div>
+        <div id="messagediv">
+          <GuestList />
+          <WatchList />
+          <SlideList />
+          <PayList />
+        </div>
+      </div>
+      <footer className="round">
+        <p>Made by Group 1</p>
+        <p>All rights reserved.</p>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
