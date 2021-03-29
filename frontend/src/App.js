2f6c62c78abac60d5b431f9560082a95d0234e0c
Aux Ciprián <60713159+cipoleon@users.noreply.github.com>
2021-03-29 20:36:20 +0200

    oldalak közti váltás alapja kész
    oldalak közti váltás alapja kész

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,53 +10,62 @@ import GuestRecordingForm from "./component/GuestRecordingForm";
 import GuestList from "./component/GuestList";
 import PayRecordingForm from "./component/PayRecordingForm";
 import PayList from "./component/PayList";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   function refreshPage() {
-    window.location.reload(false);
+    window.location.reload();
   }
   return (
     <div className={["App", "container"]}>
       <video src={bg} autoPlay="true" loop="true" muted="true" />
-      <div className="header">
-        <a href="#default" className="logo">
-          Aquapark
-        </a>
-        <div className="header-right">
-          <a className="active" href="#home">
-            Home
+      <Router history>
+        <div className="header">
+          <a href="#default" className="logo">
+            Aquapark
           </a>
-          <a href="#contact">Contact</a>
-          <a href="#about">About</a>
+          <div className="header-right">
+            <a className="active" href="#home">
+              Home
+            </a>
+            <a href="#contact">Contact</a>
+            <a href="#about">About</a>
+          </div>
         </div>
-      </div>
-      <div id="content" className="round">
-        <button id="refresh" onClick={() => refreshPage()}>
-          ↺
-        </button>
-        <div id="firstdiv">
-          <GuestRecordingForm />
-        </div>
-        <div id="firstdiv">
-          <WatchRecordingForm />
-        </div>
-        <div id="firstdiv">
-          <SlideRecordingForm />
-        </div>
-        <div id="firstdiv">
-          <PayRecordingForm />
-        </div>
-        <div id="messagediv">
-          <GuestList />
-          <WatchList />
-          <SlideList />
-          <PayList />
-        </div>
-      </div>
-      <footer className="round">
-        <p>Made by Group 1</p>
-        <p>All rights reserved.</p>
-      </footer>
+        <Switch>
+          <Route exact path="/">
+            <div>
+              <div id="content" className="round">
+                <button id="refresh" onClick={() => refreshPage()}>
+                  ↺
+                </button>
+                <div id="firstdiv">
+                  <GuestRecordingForm />
+                </div>
+                <div id="firstdiv">
+                  <WatchRecordingForm />
+                </div>
+                <div id="firstdiv">
+                  <SlideRecordingForm />
+                </div>
+                <div id="firstdiv">
+                  <PayRecordingForm />
+                </div>
+                <div id="messagediv">
+                  <GuestList />
+                  <WatchList />
+                  <SlideList />
+                  <PayList />
+                </div>
+              </div>
+              <footer className="round">
+                <p>Made by Group 1</p>
+                <p>All rights reserved.</p>
+              </footer>
+            </div>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
